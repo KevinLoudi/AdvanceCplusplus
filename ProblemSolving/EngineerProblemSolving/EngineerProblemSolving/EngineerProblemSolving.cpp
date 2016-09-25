d89c1bc67869c81de05943eb352e7b02e7fe76d5
Kevin Zhu <kevinloudi@foxmail.com>
2016-09-25 20:35:35 +0800

    Modify

--- ProblemSolving/EngineerProblemSolving/EngineerProblemSolving/EngineerProblemSolving.cpp
+++ ProblemSolving/EngineerProblemSolving/EngineerProblemSolving/EngineerProblemSolving.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <vector>
 #include "FileProcessing.h"
 #include "GnerateFile.h"
 using namespace std;
@@ -18,8 +19,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	FP.GenerateSummaryReport(iinfile);*/
 
 	GnerateFile GP;
-	string filename="Report.txt";
-	GP.GenerateBallonData(filename);
+	/*string filename="Report.txt";
+	GP.GenerateBallonData(filename);*/
+
+	/*int temp(0);
+	temp=GP.Accessmemory();
+	cout<<"The returned number: "<<temp<<endl;*/
+
+	char strg1[]="abbcfgwdbibbw", strg2[]="b";
+    vector<string> SameStr;
+
+	int tmpcount(0);
+	tmpcount=GP.AccessString(strg1,strg2);
+	cout<<"Same part count..."<<tmpcount<<endl;
 
 	system("pause");
 	return 0;
