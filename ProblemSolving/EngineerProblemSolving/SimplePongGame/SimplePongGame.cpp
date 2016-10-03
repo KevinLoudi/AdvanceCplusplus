d3fe80b7b1455f63c8e836a0623962ec5f9bd54a
KevinLoudi <zhugengyuloudi@gmail.com>
2016-10-03 13:25:21 +0800

    Add inhertance part and so on

--- ProblemSolving/EngineerProblemSolving/SimplePongGame/SimplePongGame.cpp
+++ ProblemSolving/EngineerProblemSolving/SimplePongGame/SimplePongGame.cpp
@@ -27,18 +27,18 @@ public:
 		x=originalX;y=originalY;
 		direction=STOP;
 	}
-	void changeDircetion(eDir d)
+	void changeDirection(eDir d)
 	{
 		direction=d;
 	}
 	inline int getX(){return x;}
 	inline int getY(){return y;}
-	inline eDir getDir(){return direction;}
-	void randomdirection()
+	inline eDir getDirection(){return direction;}
+	void randomDirection()
 	{
 		direction=(eDir)((rand()%6)+1);
 	}
-	void move()
+	void Move()
 	{
 		switch(direction)
 		{
@@ -149,6 +149,142 @@ public:
 		player1->Reset();
 		player2->Reset();
 	}
+	void Draw()
+	{
+		system("cls");
+		for (int i = 0; i < width + 2; i++)
+			cout << "#";
+		cout << endl;
+
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				int ballx = ball->getX();
+				int bally = ball->getY();
+				int player1x = player1->getX();
+				int player2x = player2->getX();
+				int player1y = player1->getY();
+				int player2y = player2->getY();
+
+				if (j == 0)
+					cout << "#";
+
+				if (ballx == j && bally == i)
+					cout << "O"; //ball
+				else if (player1x == j && player1y == i)
+					cout << "R"; //player1
+				else if (player2x == j && player2y == i)
+					cout << "R"; //player2
+
+				else if (player1x == j && player1y + 1 == i)
+					cout << "R"; //player1
+				else if (player1x == j && player1y + 2 == i)
+					cout << "R"; //player1
+				else if (player1x == j && player1y + 3 == i)
+					cout << "R"; //player1
+
+				else if (player2x == j && player2y + 1 == i)
+					cout << "R"; //player1
+				else if (player2x == j && player2y + 2 == i)
+					cout << "R"; //player1
+				else if (player2x == j && player2y + 3 == i)
+					cout << "R"; //player1
+				else
+					cout << " ";
+
+				if (j == width - 1)
+					cout << "#";
+			}
+			cout << endl;
+		}
+
+		for (int i = 0; i < width + 2; i++)
+			cout << "#";
+		cout << endl;
+
+		cout << "Score 1: " << score1 << endl << "Score 2: " << score2 << endl;
+	}
+
+	void Input()
+	{
+		ball->Move();
+
+		int ballx = ball->getX();
+		int bally = ball->getY();
+		int player1x = player1->getX();
+		int player2x = player2->getX();
+		int player1y = player1->getY();
+		int player2y = player2->getY();
+
+		if (_kbhit())
+		{
+			char current = _getch();
+			if (current == up1)
+				if (player1y > 0)
+					player1->moveUp();
+			if (current == up2)
+				if (player2y > 0)
+					player2->moveUp();
+			if (current == down1)
+				if (player1y + 4 < height)
+					player1->moveDown();
+			if (current == down2)
+				if (player2y + 4 < height)
+					player2->moveDown();
+
+			if (ball->getDirection() == STOP)
+				ball->randomDirection();
+
+			if (current == 'q')
+				quit = true;
+		}
+	}
+void Logic()
+	{
+		int ballx = ball->getX();
+		int bally = ball->getY();
+		int player1x = player1->getX();
+		int player2x = player2->getX();
+		int player1y = player1->getY();
+		int player2y = player2->getY();
+
+		//left paddle
+		for (int i = 0; i < 4; i++)
+			if (ballx == player1x + 1)
+				if (bally == player1y + i)
+					ball->changeDirection((eDir)((rand() % 3) + 4));
+
+		//right paddle
+		for (int i = 0; i < 4; i++)
+			if (ballx == player2x - 1)
+				if (bally == player2y + i)
+					ball->changeDirection((eDir)((rand() % 3) + 1));
+
+		//bottom wall
+		if (bally == height - 1)
+			ball->changeDirection(ball->getDirection() == DOWNRIGHT ? UPRIGHT : UPLEFT);
+		//top wall
+		if (bally == 0)
+			ball->changeDirection(ball->getDirection() == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+		//right wall
+		if (ballx == width - 1)
+			ScoreUp(player1);
+		//left wall
+		if (ballx == 0)
+			ScoreUp(player2);
+	}
+	void Run()
+	{
+		while (!quit)
+		{
+			Draw();
+			Input();
+			Logic();
+		}
+	}
+
+
 };
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -163,14 +299,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	c.move();
 	cout<<c<<endl;*/
 
-	cPaddle p1(0,0);
+	/*cPaddle p1(0,0);
 	cPaddle p2(10,0);
 	cout<<p1<<endl;
 	cout<<p2<<endl;
 	p1.moveUp();
 	p2.moveDown();
 	cout<<p1<<endl;
-	cout<<p2<<endl;
+	cout<<p2<<endl;*/
+
+	cGameManger c(40,20);
+	//c.Draw();
+	//c.Run();
 	return 0;
 }
 
--- ProblemSolving/EngineerProblemSolving/TurorialSeries/TurorialSeries.cpp
+++ ProblemSolving/EngineerProblemSolving/TurorialSeries/TurorialSeries.cpp
@@ -54,6 +54,45 @@ public:
 	}
 };
 
+
+class Parent
+{
+private:
+	int varS;
+	void printvarS();
+public:
+	int varP;
+	void printvarP();
+	Parent(){varS=5;varP=10;}
+protected:
+	int varX;
+	void printX()
+	{
+	  cout<<"X="<<varX<<endl;
+    }
+	void SetX(int x){varX=x;}
+};
+
+void Parent::printvarP()
+{
+	cout<<"P="<<varP<<endl;
+}
+void Parent::printvarS()
+{
+	cout<<"S="<<varS<<endl;
+}
+
+//everything in public section have
+//been copied to the child class
+class Child:public Parent
+{
+public:
+	void SetP(int a)
+	{
+		varP=a;
+	}
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	/*Dummy a(5);
@@ -64,6 +103,57 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout<<c<<endl;
 	//cout<<getvalue()<<endl;
 	system("pause");*/
+
+	//Dynamic Memory Allocation
+	/*cout<<sizeof(int)<<endl;
+	cout<<sizeof(double)<<endl;
+	//this is reserved when the program is started
+	int *p = new int; //the address will be store
+
+	//this is reserved when the program is complied
+	int a=10;
+	
+	*p=100;
+	cout<<*p<<endl;
+
+	delete p; //free the allocated memory
+	//plan to switch pointer to somewhere else
+	//free memory before action
+	p=&a;
+
+
+	//int A[100] = new int; //400bytes
+	int N;
+	cout<<"Enter number of elements: ";
+	cin>>N;
+
+	int *A = new int[N];
+	for (int i = 0; i < N; ++i)
+	{
+		cin>>A[i];
+	}
+	cout<<"........"<<endl;
+	for (int i = 0; i < N; ++i)
+	{
+		cout<<A[i]<<endl;
+	}
+
+    //delete with a bracket, or your only delete one element
+	delete[] A;*/
+
+	Parent P;
+	P.printvarP();
+
+	Child C;
+	C.printvarP();
+	C.SetP(30);
+	C.printvarP();
+	//C.printX(); //can not access protected section
+	P.printX();
+
+	P.printvarP();
+
+	system("pause");
 	return 0;
 }
 
