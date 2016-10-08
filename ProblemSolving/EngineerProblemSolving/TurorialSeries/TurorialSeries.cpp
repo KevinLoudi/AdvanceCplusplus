0a0e681cc4b2d3a758b37819ba5ed4fb091fc93d
KevinLoudi <zhugengyuloudi@gmail.com>
2016-10-08 15:15:27 +0800

    pointer-array, array-pointer, function-pointer

--- ProblemSolving/EngineerProblemSolving/TurorialSeries/TurorialSeries.cpp
+++ ProblemSolving/EngineerProblemSolving/TurorialSeries/TurorialSeries.cpp
@@ -93,6 +93,28 @@ public:
 	}
 };
 
+void suspension(char *s)
+{
+	cout<<s<<endl;
+	delete s;
+}
+
+void reverse(char *s)
+{
+	char *p, *q, temp;
+	p=s;
+	q=s+strlen(s)-1;
+	while(p<q)
+	{
+		temp=*p;
+		*p=*q;
+		*q=temp;
+		p++;q--;
+	}
+}
+
+int leapyear(int year);
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	/*Dummy a(5);
@@ -141,7 +163,7 @@ int _tmain(int argc, _TCHAR* argv[])
     //delete with a bracket, or your only delete one element
 	delete[] A;*/
 
-	Parent P;
+	/*Parent P;
 	P.printvarP();
 
 	Child C;
@@ -151,9 +173,99 @@ int _tmain(int argc, _TCHAR* argv[])
 	//C.printX(); //can not access protected section
 	P.printX();
 
-	P.printvarP();
+	P.printvarP();*/
 
+	int *iptr, iArray[10]={0,1,2,3,4,5,6,7,8,9};
+	double *fptr,fArray[10]={9,8,7,6,5,4,3,2,1,0};
+	int x;
+	iptr=iArray;
+	fptr=fArray;
+	//address--contents
+	for (int i = 0; i < 10; ++i)
+	{
+		cout<<i+iptr<<" "<<*(i+iptr)<<" "
+		    <<i+fptr<<" "<<*(i+fptr)<<endl;
+	}
+
+	//指针数组，一个数组里包含的全是指针
+	int *ap[2];
+	int a=23, b=45;
+	ap[0]=&a;
+	ap[1]=&b;
+	
+	//数组指针，一个指针，指向一个数组
+	int (*bp) = new int[2];
+	bp[0]=0;
+	bp[1]=1;
+
+	cout<<endl;
+	for (int i = 0; i < 2; ++i)
+	{
+		//for pointer-array, we need to use double * before retrive the contents
+		cout<<i+ap<<" "<<*(*(i+ap))<<" "
+			//whil for array-pointer, it's much simpler
+		    <<i+bp<<" "<<*(i+bp)<<endl;
+	}
+
+	//next we demostrate the "pointer-suspension", it is when two pointer actually point
+	//to the same adress, and then one deleted, the other also become void
+
+	char *p=new char[100];
+	char *s;
+	s=p;
+	strcpy(p,"C++ programming");
+	//suspension(p);
+	reverse(s);
+	cout<<s<<endl;
+	delete[] p;
+
+	//a common array
+	static char day[7][10]={
+	"Su","Mo","Tu","We","TH","FR","Sa" 
+	};
+
+	//a point array, each element contain a pointer to a string
+	static char *dday[7]={
+		"Su","Mo","Tu","We","TH","FR","Sa"
+	};
+
+	for (int i = 0; i < 7; ++i)
+	{
+		//*(dday[i]+i) == day[i][i]
+		cout<<i<<" "<<day[i]+0<<" "<<*(dday[i]+1)<<endl;
+	}
+
+	/*function-pointer===================================*/
+	system("cls"); 
+	int year,result;
+	//declare a int type function
+	int (*fnptr)(int);
+	//direct the pointer to a specific function
+	fnptr=leapyear;
+	cout<<"Pleasr input a year:";
+	cin>>year;
+
+	//make use of the function pointer
+	result=(*fnptr)(year);
+	if (result==0)
+	{
+		cout<<year<<" is a leapyear";
+	}else
+	{
+		cout<<year<<" is not a leapyear";
+	}
+    /*============================================== */
 	system("pause");
 	return 0;
 }
 
+int leapyear(int year)
+{
+	if (((year%4==0)&&(year%100!=0))||(year%400==0))
+	{
+		return 1;
+	}else
+	{
+		return 0;
+	}
+}
